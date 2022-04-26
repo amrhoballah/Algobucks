bfbf6f91e8b7ce8d31385edb932aef1617ed90e6
amrhoballah <amrhoballah@yahoo.com>
2022-04-26 05:44:47 +0200

    minor updates

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ehr-2.0'`, () => {
+  it(`should have as title 'Algobucks'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ehr-2.0');
+    expect(app.title).toEqual('Algobucks');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ehr-2.0 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('algobucks app is running!');
   });
 });
